06ffc3c95a8ea362610a218a394ca4900df8d2c6
m-hayabusa <s.hashimoto@hs-sh.net>
2023-02-03 11:21:33 +0000

    デフォルトテーマの変更

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -298,8 +298,8 @@ interface Watcher {
  * 常にメモリにロードしておく必要がないような設定情報を保管するストレージ(非リアクティブ)
  */
 import { miLocalStorage } from './local-storage';
-import lightTheme from '@/themes/l-light.json5';
-import darkTheme from '@/themes/d-green-lime.json5';
+import lightTheme from '@/themes/l-sushi.json5';
+import darkTheme from '@/themes/d-green-orange.json5';
 import { Note, UserDetailed } from 'misskey-js/built/entities';
 
 export class ColdDeviceStorage {
